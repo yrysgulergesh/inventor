200c412b6c29986ec1b2f82d80a1886e44c17acf
Yrysgul <yrysgulergesh@gmail.com>
2021-11-22 16:28:09 +0600

    add adding

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Jinja, Templates 48 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,3 +8,15 @@ def homepage():
     f = open('goods.txt', 'r', encoding='utf-8')
     txt = f.readlines()
     return render_template('index.html', goods=txt)
+
+
+@app.route('/add/', methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open('goods.txt',  'a+', encoding='utf-8')
+    f.write(good + "\n")
+    f.close()
+    return """
+    <h1>Инвентарь пополнен</h1>
+    <a href='/'>Домой</a>
+    """
